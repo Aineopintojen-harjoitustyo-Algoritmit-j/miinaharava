78eeeac7bd114163418bf5da2897aed62794a09f
Aineopintojen-harjoitustyo-Algoritmit-j <github-hy-tiralabra@v.hix.fi>
2024-02-17 10:06:54 +0200

    Root directory can be now run in any cwd.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,6 @@
 """ __main__.py - Käynnistellään ohjelma src/miinaharava kansiosta """
 from runpy import run_path
-run_path("src/miinaharava", run_name="miinaharava")
+from pathlib import Path
+
+my_path = Path(__file__).parent.resolve()
+run_path(f"{my_path}/src/miinaharava", run_name="miinaharava")
